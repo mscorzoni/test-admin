481a7edf67330af558cd39cdc2bd6e43fa18688f
Marcos Scorzoni <mlessa@gmail.com>
2018-09-23 12:47:13 +0100

    add post list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import { PostList } from './posts';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
-const App = () => <Admin dataProvider={dataProvider} />;
+const App = () => (<Admin dataProvider={dataProvider}>
+                    <Resource name="posts" list={PostList} />
+                   </ Admin>);
 export default App;
